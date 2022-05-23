0ec5d879a0dad97a8ddc769f77e896c0f682306c
ASelmane <adrien.selmane@gmail.com>
2022-05-23 14:09:27 +0200

    Index: fetch json data + add assets

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country,tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers_ID/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +9,17 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city + ', ' + country;
+        const p = document.createElement( 'p' );
+        p.textContent = tagline;
+        const span = document.createElement( 'span' );
+        span.textContent = price + '€/jour ';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(span);
         return (article);
     }
     return { name, picture, getUserCardDOM }
