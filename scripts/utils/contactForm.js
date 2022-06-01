a21f33ffd9ebf3aefd5e645695810f3ffb2ae19a
ASelmane <adrien.selmane@gmail.com>
2022-06-01 12:53:26 +0200

    Contact: create contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,23 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+	modal.style.display = "flex";
+    document.body.style.overflow='hidden';
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    document.body.style.overflow='auto';
 }
+
+const form = document.getElementById("form");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Prénom: ' + form.firstname.value)
+    console.log('Nome: ' + form.lastname.value)
+    console.log('Email: ' + form.email.value)
+    console.log('Message: ' + form.message.value)
+    closeModal();
+    form.reset();
+})
